b1f0e04e22276c3fdd30f0d6144476c93538f7e3
Nezarn <nezarno90@gmail.com>
2024-11-23 12:28:40 +0100

    v1.1
    - Add missing calling convention (fixes x86 crash)
    - Add an optional workaround for eyeshine flickering

--- README.md
+++ README.md
@@ -5,15 +5,17 @@ What this thing does:
 - Only returning extensions game requires, this fixes crashes (including the crash related to compressed textures on RTX cards)
 - By enabling "Enable 2.0++ on AMD/ATI" in TCoRFix.ini, shader mode 2.0++ becomes selectable on AMD GPUs ~~until amd decides to break their driver~~
 - Fix GL_MAX_TEXTURE_UNITS error on RTX cards (optional fix, enable "Fix Misc Bugs" in TCoRFix.ini)
+- Adds a workaround for eyeshine flickering
 
 ## How to Install
 1. Download latest [release](https://github.com/Nezarn/TCoRFix/releases)
-2. Extract to ```<path-to-game>\System\Win64_AMD64\```
-3. Download latest [Ultimate-ASI-Loader_x64.zip](https://github.com/ThirteenAG/Ultimate-ASI-Loader/releases)
-4. Extract dinput8.dll to ```<path-to-game>\System\Win64_AMD64\``` and rename it to winmm.dll
+2. Extract to ```<path-to-game>\System\Win64_AMD64\``` or ```<path-to-game>\System\Win32_x86<_SSE/SSE2>```
+3. Download latest [Ultimate-ASI-Loader_x64.zip or Ultimate-ASI-Loader](https://github.com/ThirteenAG/Ultimate-ASI-Loader/releases)
+4. Extract dinput8.dll to ```<path-to-game>\System\Win64_AMD64\``` or ```<path-to-game>\System\Win32_x86<_SSE/SSE2>``` and rename it to winmm.dll
 5. Play
 
 ## Building from source
+x64:
 ```
 git clone https://github.com/Nezarn/TCoRFix
 cd TCoRFix
@@ -21,6 +23,11 @@ mkdir build && cd build
 cmake .. -A x64
 cmake --build . --config Release
 ```
-
-## NOTE
-This was only tested on x64 version of the game (but technically it should work with x86 too)
+x86:
+```
+git clone https://github.com/Nezarn/TCoRFix
+cd TCoRFix
+mkdir build && cd build
+cmake .. -A Win32
+cmake --build . --config Release
+```
--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,15 @@
 #include <iostream>
 #pragma comment(lib, "opengl32.lib")
 
-// These are defined in glext.h, but its kinda waste to include just for these 2
+// These are defined in glext.h, but its kinda waste to include just for these 3
 #define GL_MAX_TEXTURE_UNITS              0x84E2
 #define GL_MAX_TEXTURE_IMAGE_UNITS        0x8872
+#define GL_FRAGMENT_PROGRAM_ARB           0x8804
 
 // Settings
 bool fixMiscBugs = false;
 bool enableNVonATI = false;
+bool flickerWorkaround = false;
 
 // Buffer returned for glGetString(GL_EXTENSIONS)
 GLubyte ret[1024];
@@ -20,6 +22,11 @@ GLubyte ret[1024];
 // Original functions
 void (WINAPI* glGetIntegerv_ori)(GLenum pname, GLint* data);
 const GLubyte* (WINAPI* glGetString_ori)(GLenum name);
+PROC (WINAPI* wglGetProcAddress_ori)(LPCSTR procName);
+
+// OpenGL ARB functions
+typedef void (APIENTRY *PFNGLPROGRAMSTRINGARBPROC)(GLenum target, GLenum format, GLsizei len, const void *string);
+PFNGLPROGRAMSTRINGARBPROC glProgramStringARB = NULL;
 
 void WINAPI glGetIntegerv_hook(GLenum pname, GLint* data)
 {
@@ -34,7 +41,7 @@ void WINAPI glGetIntegerv_hook(GLenum pname, GLint* data)
 	glGetIntegerv_ori(pname, data);
 }
 
-const GLubyte* glGetString_hook(GLenum name)
+const GLubyte* WINAPI glGetString_hook(GLenum name)
 {
 	if (name == GL_EXTENSIONS)
 	{
@@ -51,7 +58,7 @@ const GLubyte* glGetString_hook(GLenum name)
 			extList = "GL_ARB_FRAGMENT_PROGRAM GL_ARB_MULTITEXTURE GL_ARB_TEXTURE_COMPRESSION GL_ARB_TEXTURE_CUBE_MAP GL_ARB_TEXTURE_ENV_COMBINE GL_ARB_TEXTURE_ENV_DOT3 "
 					"GL_ARB_VERTEX_BUFFER_OBJECT GL_ARB_VERTEX_PROGRAM GL_EXT_BGRA GL_EXT_SECONDARY_COLOR GL_EXT_STENCIL_TWO_SIDE GL_EXT_STENCIL_WRAP GL_EXT_TEXTURE_COMPRESSION_S3TC "
 					"GL_EXT_TEXTURE_FILTER_ANISOTROPIC GL_EXT_TEXTURE_LOD_BIAS GL_NV_COPY_DEPTH_TO_COLOR GL_NV_FENCE GL_NV_FRAGMENT_PROGRAM GL_NV_FRAGMENT_PROGRAM_OPTION "
-					"GL_NV_FRAGMENT_PROGRAM2 GL_NV_OCCLUSION_QUERY GL_NV_REGISTER_COMBINERS GL_NV_register_combiners2 GL_NV_TEXTURE_SHADER GL_NV_VERTEX_ARRAY_RANGE GL_NV_VERTEX_PROGRAM";
+					"GL_NV_FRAGMENT_PROGRAM2 GL_NV_OCCLUSION_QUERY GL_NV_REGISTER_COMBINERS GL_NV_REGISTER_COMBINERS2 GL_NV_TEXTURE_SHADER GL_NV_VERTEX_ARRAY_RANGE GL_NV_VERTEX_PROGRAM";
 		}
 		else if ((pos = vendorstr.find("ATI")) != std::string::npos) // AMD still returns ATI Technologies Inc.
 		{
@@ -82,12 +89,46 @@ const GLubyte* glGetString_hook(GLenum name)
 	return ret;
 }
 
+void APIENTRY glProgramStringARB_hook(GLenum target, GLenum format, GLsizei len, const void* string)
+{
+	if(target == GL_FRAGMENT_PROGRAM_ARB && len == 9467) // XRShader_FP20SS_SpecNormal.fp
+	{
+		std::string prog = reinterpret_cast<const char*>(string);
+		std::string tofind = "DP4 r0.a, smpacked0, 0.125;";
+		size_t pos = prog.find(tofind);
+		if (pos != std::string::npos)
+		{
+			// Kinda hacky but seems to work.
+			prog.replace(pos, tofind.length(), "DP4 r0.a, smpacked0, 0.125;\nNRM r0, r0;\nNRM r0, r0;");
+			glProgramStringARB(target, format, strlen(prog.c_str()), prog.c_str());
+			
+			return;
+		}
+	}
+
+	glProgramStringARB(target, format, len, string);
+}
+
+PROC WINAPI wglGetProcAddress_hook(LPCSTR procName)
+{
+	if(strcmp("glProgramStringARB", procName) == 0)
+	{
+		// Get the ProcAddress for glProgramStringARB
+		glProgramStringARB = (PFNGLPROGRAMSTRINGARBPROC)wglGetProcAddress_ori("glProgramStringARB");
+		// Return our hook
+		return (PROC)glProgramStringARB_hook;
+	}
+
+	return wglGetProcAddress_ori(procName);
+}
+
 void Init()
 {
 	// Read settings from ini
 	const char* ini = ".\\TCoRFix.ini";
 	fixMiscBugs = GetPrivateProfileIntA("General", "Fix Misc Bugs", 0, ini);
 	enableNVonATI = GetPrivateProfileIntA("General", "Enable 2.0++ on AMD/ATI", 0, ini);
+	flickerWorkaround = GetPrivateProfileIntA("General", "Eyeshine Flicker Workaround", 0, ini);
 
 	// Preload ogl dll (in case its not loaded yet, so minhook can always work)
 	LoadLibraryA("opengl32.dll");
@@ -99,6 +140,11 @@ void Init()
 	if (fixMiscBugs)
 	{
 		MH_CreateHookApi(L"opengl32.dll", "glGetIntegerv", glGetIntegerv_hook, (void**)&glGetIntegerv_ori);
+	}
+
+	if (flickerWorkaround)
+	{
+		MH_CreateHookApi(L"opengl32.dll", "wglGetProcAddress", wglGetProcAddress_hook, (void**)&wglGetProcAddress_ori);
 	}
 
 	MH_EnableHook(MH_ALL_HOOKS);
